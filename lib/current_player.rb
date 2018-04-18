0acae061a189133f1f1897d7cc73570794ce1e5a
Adriana Perez <adriana.perez.garcia@outlook.com>
2018-04-18 18:01:35 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,7 +9,7 @@ return counter
 end
 
 def current_player(board)
-  if turn_count(board).even? ? current_player = "X" : current_player = "O"#% 2 ==0
+  input_type = turn_count(board).even? ? current_player = "X" : current_player = "O"#% 2 ==0
     #current_player = "X"
   #else
     #current_player = "O"
